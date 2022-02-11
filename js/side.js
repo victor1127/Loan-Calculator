31792e4296b362151fc95ff122868ab49fd7e330
victor1127 <95936106+victor1127@users.noreply.github.com>
2022-02-11 16:58:56 -0400

    Updating template

--- js/side.js
+++ js/side.js
@@ -1,4 +1,22 @@
-function DisplayHelloWorld()
+class LoanModel
 {
-    alert("Hello world");
+    constructor(loanAmount,term,interestRate)
+    {
+        this.LoanAmount=loanAmount;
+        this.Term=term;
+        this.InterestRate=interestRate;
+    }
+
 }
+
+
+function GetInitialValues()
+{
+    let Loan = new LoanModel();
+    Loan.loanAmount =  document.getElementById("loanAmount").value;
+    Loan.Term =  document.getElementById("term").value;
+    Loan.InterestRate =  document.getElementById("term").value;
+    
+}
+
+

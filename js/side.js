6efdd661c2709cef0d55af21f6cde4a935a196cf
victor1127 <95936106+victor1127@users.noreply.github.com>
2022-02-13 00:03:36 -0400

    Adding js updates

--- js/side.js
+++ js/side.js
@@ -1,7 +1,8 @@
 function GetValues()
 {
     let loan = GetInitialValues(new LoanModel());
-    GetMonthlyPayment(loan);
+    let calculator = new LoanCalculator(loan);
+    let test = calculator.GetMonthlyPayment();
 }
 
 function LoanModel()
@@ -10,7 +11,7 @@ function LoanModel()
     this.Term;
     this.InterestRate;
 }
-
+//loanAmortiguacion
 function GetInitialValues(LoanModel)
 {
     LoanModel.LoanAmount = document.getElementById("loanAmount").value,
@@ -20,9 +21,43 @@ function GetInitialValues(LoanModel)
     
 }
 
-function GetMonthlyPayment(loan)
+function DisplayContent(LoanCalculator)
 {
-    //Calcular cuota préstamo = (Monto * (TEM x (1 + TEM) ^ n)) / ((1 + TEM) ^ n) - 1)
-    let monthlyPayment = 
+    let monthlyPayment = document.getElementById("resultMonthlyP");
+    monthlyPayment.innerHTML=
+}
+
+
+
+//Classes-----------------------------------------------------------------------------------
+
+class LoanCalculator
+{
+    constructor(LoanModel)
+    {
+        this.LoanAmount = LoanModel.LoanAmount;
+        this.TotalPrincipal = LoanModel.LoanAmount;
+        this.Term = LoanModel.Term;
+        this.InterestRate = LoanModel.InterestRate/100;
+        this.MonthlyPayment;
+        this.TotalInterest;
+        this.TotalCost;
+        this.Principal;
+        this.InterestRate;
+        this.Balance;
+    }
+
+    GetMonthlyPayment()
+    {
+        let rate = this.GetMonthlyInterestPercentage();
+        let monthlyPayment =  (rate*this.LoanAmount)/(1-(1+rate)**-60);
+        return monthlyPayment.toFixed(2);
+    }
+
+    GetMonthlyInterestPercentage()
+    {
+        let rate = (1 + this.InterestRate)**(1/12);
+        return rate-1;
+    }
 
 }
